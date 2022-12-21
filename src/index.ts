ab0557d135fa71705a85406055b701837754148e
Jan Nicklas <j.nicklas@me.com>
2022-12-21 18:22:32 +0100

    feat: resolve relative calls to importScripts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 // Patch Worker to allow loading scripts from remote URLs
-// 
+//
 // It's a workaround for the fact that the Worker constructor
 // accepts only local URLs, not remote URLs:
-// https://developer.mozilla.org/en-US/docs/Web/API/Worker/Worker 
+// https://developer.mozilla.org/en-US/docs/Web/API/Worker/Worker
 //
 // As a workaround this patched Worker constructor will
 // use `importScripts` to load the remote script.
@@ -14,18 +14,30 @@
 // ```ts
 // import "remote-web-worker";
 // ```
-typeof window !== "undefined" && (
-    window.Worker = ((BaseWorker: typeof window.Worker) =>
-        class Worker extends BaseWorker {
-            constructor(scriptURL: string | URL, options?: WorkerOptions) {
-                if (/^(http|\/\/)/.test(String(scriptURL))) {
-                    super(URL.createObjectURL(new Blob([`importScripts( "${scriptURL}" );`], { type: "text/javascript" })));
-                } else {
-                    super(scriptURL, options);
-                }
-            }
+typeof window !== "undefined" &&
+  (window.Worker = ((BaseWorker: typeof window.Worker) =>
+    class Worker extends BaseWorker {
+      constructor(scriptURL: string | URL, options?: WorkerOptions) {
+        if (/^(http|\/\/)/.test(String(scriptURL))) {
+          super(
+            URL.createObjectURL(
+              new Blob(
+                [
+                  // Load the remote script using `importScripts`
+                  // and replace the `importScripts` function with
+                  // a patched version that will resolve relative URLs
+                  // to the remote script URL.
+                  `importScripts=((baseImportScripts)=>(...args)=>baseImportScripts(...args.map((url)=>''+new URL(url,"${scriptURL}"))))(importScripts);importScripts("${scriptURL}")`,
+                ],
+                { type: "text/javascript" }
+              )
+            ),
+            options
+          );
+        } else {
+          super(scriptURL, options);
         }
-    )(window.Worker)
-);
+      }
+    })(window.Worker));
 
 export type WorkerConstructor = typeof window.Worker;
